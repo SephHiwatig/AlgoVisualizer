e286d24935b968941eef681d763c90f6acaa02c2
Joseph Hiwatig <jhiwatig@itrlab.com>
2020-06-19 13:30:28 -0400

    Fixed Navbar toggle on mobile

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"],
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // Hide the dropdown menu for mobile when window is desktop size
+    window.addEventListener("resize", () => {
+      const width = window.innerWidth;
+      if (width >= 768) {
+        const menu = document.querySelector(".nav-menu-mobile") as HTMLElement;
+        menu.style.display = "none";
+      }
+    });
   }
 
+  // Dropdown menu toggler for mobile
+  onToggleMobileMenu() {
+    let menu = <HTMLElement>document.querySelector(".nav-menu-mobile");
+    if (menu.style.display !== "block") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
 }
