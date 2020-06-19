9e3bd745f0d1eb3b253448dcb46f5cfd75ce9924
Joseph Hiwatig <jhiwatig@itrlab.com>
2020-06-19 14:19:06 -0400

    Added Sort Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,20 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
+import { SelectionComponent } from "./sort/selection/selection.component";
+import { BubbleComponent } from "./sort/bubble/bubble.component";
+import { InsertionComponent } from "./sort/insertion/insertion.component";
+import { QuickComponent } from "./sort/quick/quick.component";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    SelectionComponent,
+    BubbleComponent,
+    InsertionComponent,
+    QuickComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
