d0a7d641a8e70d0bac4c670a3d9f64732338cb61
Joseph Hiwatig <jhiwatig@itrlab.com>
2020-06-19 14:54:01 -0400

    Added Search/Path Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+
+// Routing
 import { AppRoutingModule } from "./app.routing";
 
+// Components
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
 import { SelectionComponent } from "./sort/selection/selection.component";
 import { BubbleComponent } from "./sort/bubble/bubble.component";
 import { InsertionComponent } from "./sort/insertion/insertion.component";
 import { QuickComponent } from "./sort/quick/quick.component";
+import { LinearComponent } from "./search/linear/linear.component";
+import { BinaryComponent } from "./search/binary/binary.component";
+import { JumpComponent } from "./search/jump/jump.component";
+import { DijkstraComponent } from "./path/dijkstra/dijkstra.component";
+import { AstarComponent } from "./path/astar/astar.component";
 
 @NgModule({
   declarations: [
@@ -17,6 +25,11 @@ import { QuickComponent } from "./sort/quick/quick.component";
     BubbleComponent,
     InsertionComponent,
     QuickComponent,
+    LinearComponent,
+    BinaryComponent,
+    JumpComponent,
+    DijkstraComponent,
+    AstarComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/path/dijkstra/dijkstra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dijkstra',
+  templateUrl: './dijkstra.component.html',
+  styleUrls: ['./dijkstra.component.css']
+})
+export class DijkstraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
