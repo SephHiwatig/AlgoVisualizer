3ad2ce7ff54002fd85887211cad73089ad18dea9
Joseph Hiwatig <jhiwatig@itrlab.com>
2020-06-19 16:42:37 -0400

    Added Selection Sort Info/layout

--- src/app/sort/selection/selection.component.ts
+++ src/app/sort/selection/selection.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-selection',
-  templateUrl: './selection.component.html',
-  styleUrls: ['./selection.component.css']
+  selector: "app-selection",
+  templateUrl: "./selection.component.html",
+  styleUrls: ["./selection.component.css"],
 })
 export class SelectionComponent implements OnInit {
+  arrayToSort = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // Populate the array to sort
+    for (let i = 0; i < 100; i++) {
+      this.arrayToSort.push(Math.ceil(Math.random() * 100));
+    }
   }
-
 }
