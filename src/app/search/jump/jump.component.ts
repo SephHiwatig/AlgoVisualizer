9f8fbb7e95c2815308bc02332fc20d7b103ecc95
Joseph Hiwatig <jhiwatig@itrlab.com>
2020-06-26 11:19:17 -0400

    Added Jump Search Template

--- src/app/search/jump/jump.component.ts
+++ src/app/search/jump/jump.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SearchSeervice } from "../search.service";
 
 @Component({
-  selector: 'app-jump',
-  templateUrl: './jump.component.html',
-  styleUrls: ['./jump.component.css']
+  selector: "app-jump",
+  templateUrl: "./jump.component.html",
+  styleUrls: ["./jump.component.css"],
 })
 export class JumpComponent implements OnInit {
+  languageUrl;
 
-  constructor() { }
+  constructor(private searchService: SearchSeervice) {}
 
   ngOnInit() {
+    this.languageUrl = this.searchService.languageSelect("cplusplus", "jump");
   }
 
+  onLanguageSelect(language) {
+    this.languageUrl = this.searchService.languageSelect(language, "jump");
+  }
 }
